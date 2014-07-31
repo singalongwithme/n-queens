31e4bf45ba4300d3a0a36c9a9145803b6c73f50f
singalongwithme <csinghavong@gmail.com>
2014-07-30 21:47:01 -0700

    Stuck one n rooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,8 +14,17 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n : n});
+  rowIdx = 0;
+  colIdx = 0;
 
+  for (var i = 0; i < n; i++) {
+    board.togglePiece(i,i);
+    rowIdx++;
+    colIdx++;
+  }
+
+  var solution = board.rows();
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -24,13 +33,35 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var newEmptyBoard = new Board({n:n});
+  console.log(newEmptyBoard.rows());
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  // make board fn to recurse through board
+  var makeBoard = function(parentBoard, rowNumber){
+    if (rowNumber >= n ){
+      console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+      return solutionCount;
+    } else {
+      for( var i = 0; i < n; i++ ){
+        console.log(rowNumber, i);
+        console.log(parentBoard.rows());
+        parentBoard.togglePiece(rowNumber, i);
+        if( !parentBoard.hasColConflictAt(i) ){
+          var newChildBoard = new Board(parentBoard.rows());
+          // newChildBoard.togglePiece(rowNumber, i);
+          console.log(newChildBoard.rows());
+          // Count last rows toggles as a proper solution
+          if (rowNumber === n - 1) {
+            solutionCount++;
+          }
+          return makeBoard(newChildBoard, rowNumber + 1);
+        }
+      }
+    }
+  }
+  return makeBoard(newEmptyBoard, 0);
 };
-
-
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
